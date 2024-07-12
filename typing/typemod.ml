0206877c2192859ff607e0532c71c6c3897ad00c
samsa1 <samuel.aime.vivien@free.fr>
2024-07-12 15:15:29 +0200

    Added inference mechanism for implicit module expression

--- typing/typemod.ml
+++ typing/typemod.ml
@@ -2387,7 +2387,19 @@ let check_purity loc env funct_body pure =
     raise (Error (loc, env, Not_allowed_in_functor_body Impure))
   | _ -> ()
 
-let rec type_module ?(alias=false) ~strengthen ~funct_body anchor env smod =
+let rec infer_implicit ~loc env mty =
+  let mexp = Implicitmod.infer ~loc env mty in
+  let (mtexp, shape) =
+      type_module ~strengthen:true ~funct_body:Pure None env mexp in
+  let md, _final_shape =
+    wrap_constraint_with_shape env true mtexp mty shape Tmodtype_implicit
+  in
+  { md with
+    mod_loc = loc;
+    mod_attributes = [];
+  }
+
+and type_module ?(alias=false) ~strengthen ~funct_body anchor env smod =
   Builtin_attributes.warning_scope smod.pmod_attributes
     (fun () -> type_module_aux ~alias ~strengthen ~funct_body anchor env smod)
 
@@ -3161,6 +3173,8 @@ and type_str_item ~names ~toplevel ~funct_body anchor env shape_map
         Tstr_attribute x, [], shape_map, env
   in
   { str_desc = desc; str_loc = loc; str_env = env }, sg, shape_map, new_env
+
+let _ = infer_implicit
 
 let type_toplevel_phrase env s =
   Env.reset_required_globals ();
