02e59be6ba582a055ccd1cfe20e8c7c287758f8e
Olivier Nicole <olivier@chnik.fr>
2026-04-09 18:01:20 +0200

    Add Format.pp_print_iarray
    As suggested in #14320

--- testsuite/tests/lib-format/print_array.ml
+++ testsuite/tests/lib-format/print_array.ml
@@ -15,6 +15,9 @@ let say s = Printf.printf (s ^^ "\n%!");;
 
 let pp_print_intarray = pp_print_array ~pp_sep:(fun fmt () -> pp_print_char fmt ' ') pp_print_int;;
 
+let pp_print_intiarray =
+  pp_print_iarray ~pp_sep:(fun fmt () -> pp_print_char fmt ' ') pp_print_int;;
+
 let () =
 
   say "empty";
@@ -24,5 +27,18 @@ let () =
   test (asprintf "%a" pp_print_intarray [| 0 |]       = "0");
   test (asprintf "%a" pp_print_intarray [| 0; 1; 2 |] = "0 1 2");
   test (asprintf "%a" pp_print_intarray [| 0; 0 |]    = "0 0");
+
+  say "\nend of tests"
+
+let () =
+
+  say "empty";
+  test (asprintf "%a" pp_print_intiarray [||] = "");
+
+  say "\nmisc";
+  test (asprintf "%a" pp_print_intiarray [| 0 |]       = "0");
+  test
+    (asprintf "%a" pp_print_intiarray ([| 0; 1; 2 |] : int iarray) = "0 1 2");
+  test (asprintf "%a" pp_print_intiarray [| 0; 0 |]    = "0 0");
 
   say "\nend of tests"
