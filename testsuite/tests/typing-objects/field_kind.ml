5038598cac427ff1175eff1ed27877371220a32d
samsa1 <samuel.aime.vivien@free.fr>
2026-04-15 10:54:19 +0200

    Added test with changing private of object methods in moregen and eqtype

--- testsuite/tests/typing-objects/field_kind.ml
+++ testsuite/tests/typing-objects/field_kind.ml
@@ -7,6 +7,7 @@ type _ t = Int : int t;;
 type _ t = Int : int t
 |}]
 
+(** Test unification of kinds *)
 let o =
   object (self)
     method private x = 3
@@ -73,4 +74,233 @@ Warning 15 [implicit-public-methods]: the following private methods were made
 
 val o3 : < m : 'a -> int; x : int > as 'a = <obj>
 val aargh : unit = ()
+|}]
+
+module type T = sig type t end
+let type_of (type r) (x:r) = (module struct type t = r end: T with type t = r)
+
+[%%expect{|
+module type T = sig type t end
+val type_of : 'r -> (module T with type t = 'r) = <fun>
+|}]
+
+(** Test moregen of kinds *)
+let moregen_public_public = object (self)
+  method n = 0
+  initializer
+     let module M = (val type_of self) in
+     let module N: sig val s: unit -> M.t end = struct
+       let s () : <n : int> = assert false
+     end
+     in
+     ()
+end
+
+[%%expect{|
+val moregen_public_public : < n : int > = <obj>
+|}, Principal{|
+Line 4, characters 20-38:
+4 |      let module M = (val type_of self) in
+                        ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+
+val moregen_public_public : < n : int > = <obj>
+|}]
+
+let moregen_public_private = object (self)
+  method private n = 0
+  initializer
+     let module M = (val type_of self) in
+     let module N: sig val s: unit -> M.t end = struct
+       let s () : <n : int> = assert false
+     end
+     in
+     ()
+end
+
+[%%expect{|
+Uncaught exception: Ctype.Public_method_to_private_method
+
+|}, Principal{|
+Line 4, characters 20-38:
+4 |      let module M = (val type_of self) in
+                        ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+Uncaught exception: Ctype.Public_method_to_private_method
+
+|}]
+
+let moregen_private_public = object (self)
+  method private n = 0
+  initializer
+     let module M = (val type_of self) in
+     let module N: sig val s: unit -> <n : int> end = struct
+       let s () : M.t = assert false
+     end
+     in
+     ()
+end
+
+[%%expect{|
+Lines 1-10, characters 29-3:
+ 1 | .............................object (self)
+ 2 |   method private n = 0
+ 3 |   initializer
+ 4 |      let module M = (val type_of self) in
+ 5 |      let module N: sig val s: unit -> <n : int> end = struct
+ 6 |        let s () : M.t = assert false
+ 7 |      end
+ 8 |      in
+ 9 |      ()
+10 | end
+Warning 15 [implicit-public-methods]: the following private methods were made
+  public implicitly: "n".
+
+val moregen_private_public : < n : int > = <obj>
+|}, Principal{|
+Line 4, characters 20-38:
+4 |      let module M = (val type_of self) in
+                        ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+
+Lines 1-10, characters 29-3:
+ 1 | .............................object (self)
+ 2 |   method private n = 0
+ 3 |   initializer
+ 4 |      let module M = (val type_of self) in
+ 5 |      let module N: sig val s: unit -> <n : int> end = struct
+ 6 |        let s () : M.t = assert false
+ 7 |      end
+ 8 |      in
+ 9 |      ()
+10 | end
+Warning 15 [implicit-public-methods]: the following private methods were made
+  public implicitly: "n".
+
+val moregen_private_public : < n : int > = <obj>
+|}]
+
+let moregen_private_private = object (self)
+  method private n = 0
+  initializer
+     let module M1 = (val type_of self) in
+     let module M2 = (val type_of self) in
+     let module N: sig val s: unit -> M1.t end = struct
+       let s () : M2.t = assert false
+     end
+     in
+     ()
+end
+
+[%%expect{|
+val moregen_private_private : <  > = <obj>
+|}, Principal{|
+Line 4, characters 21-39:
+4 |      let module M1 = (val type_of self) in
+                         ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+
+Line 5, characters 21-39:
+5 |      let module M2 = (val type_of self) in
+                         ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+
+val moregen_private_private : <  > = <obj>
+|}]
+
+(** Test eqtype of kinds *)
+let eqtype_public_public = object (self)
+  method n = 0
+  method k: unit =
+     let module M = (val type_of self) in
+     let module N: sig type t = M.t end = struct
+             type t = <n: int; k:unit >
+     end
+     in
+     ()
+end
+
+[%%expect{|
+val eqtype_public_public : < k : unit; n : int > = <obj>
+|}, Principal{|
+Line 4, characters 20-38:
+4 |      let module M = (val type_of self) in
+                        ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+
+val eqtype_public_public : < k : unit; n : int > = <obj>
+|}]
+
+let eqtype_public_private = object (self)
+  method private n = 0
+  method k: unit =
+     let module M = (val type_of self) in
+     let module N: sig type t = M.t end = struct
+             type t = <n: int; k:unit >
+     end
+     in
+     ()
+end
+
+[%%expect{|
+Uncaught exception: Ctype.Unify_trace(0)
+
+|}, Principal{|
+Line 4, characters 20-38:
+4 |      let module M = (val type_of self) in
+                        ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+Uncaught exception: Ctype.Unify_trace(0)
+
+|}]
+
+let eqtype_private_public = object (self)
+  method private n = 0
+  method k: unit =
+     let module M = (val type_of self) in
+     let module N: sig type t = <n: int; k:unit > end = struct
+             type t = M.t
+     end
+     in
+     ()
+end
+
+[%%expect{|
+Uncaught exception: Ctype.Unify_trace(0)
+
+|}, Principal{|
+Line 4, characters 20-38:
+4 |      let module M = (val type_of self) in
+                        ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+Uncaught exception: Ctype.Unify_trace(0)
+
+|}]
+
+let eqtype_private_private = object (self)
+  method private n = 0
+  method k: unit =
+     let module M1 = (val type_of self) in
+     let module M2 = (val type_of self) in
+     let module N: sig type t = M2.t end = struct
+             type t = M1.t
+     end
+     in
+     ()
+end
+
+[%%expect{|
+val eqtype_private_private : < k : unit > = <obj>
+|}, Principal{|
+Line 4, characters 21-39:
+4 |      let module M1 = (val type_of self) in
+                         ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+
+Line 5, characters 21-39:
+5 |      let module M2 = (val type_of self) in
+                         ^^^^^^^^^^^^^^^^^^
+Warning 18 [not-principal]: this module unpacking is not principal.
+
+val eqtype_private_private : < k : unit > = <obj>
 |}]
