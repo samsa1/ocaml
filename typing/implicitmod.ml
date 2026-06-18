ebbb4331018970af9f71245dd464052530a63d8b
samsa1 <samuel.aime.vivien@free.fr>
2026-06-18 16:00:01 +0200

    Implemented subtyping approximation

--- typing/implicitmod.ml
+++ typing/implicitmod.ml
@@ -352,7 +352,11 @@ let build_solution ~loc {id; env; signature} name path args =
   let snap = Btype.snapshot () in
   let rec build_mexp pme path tme = function
     | [] ->
-      begin
+      if not (Includemod.approx_modtypes env tme.Typedtree.mod_type signature)
+      then begin
+        Btype.backtrack snap;
+        None
+      end else begin
         match
           Includemod.modtypes_collect_constraint ~loc ~mark:false env
               tme.Typedtree.mod_type signature
@@ -567,6 +571,9 @@ and filter_identifiers d ~loc trace ctxt_constraints problem next =
     in
     match extract_function d problem.env problem.nargs mdecl with
     | None ->
+      filter_identifiers d ~loc trace ctxt_constraints problem rest
+    | Some (_, env_result, result)
+        when not (Includemod.approx_modtypes env_result result problem.signature) ->
       filter_identifiers d ~loc trace ctxt_constraints problem rest
     | Some (arguments, env_result, result) ->
       let snap = Btype.snapshot () in
